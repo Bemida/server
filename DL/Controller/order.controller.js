1d969b5cf2b3a1d8a485885a43ea41021a8568ee
Rotem Rogers <rotemrogers@gmail.com>
2023-06-25 18:16:18 +0300

    orderSchema

---
+++ DL/Controller/order.controller.js
@@ -0,0 +1,20 @@
+const orderModel = require ('../Model/order.model')
+
+//This is where you'll write your CRUD functions for the database - Create, Read, Update, Delete
+
+
+async function create(data){
+    return await orderModel.create(data)
+}
+async function read(filter = {}){
+await orderModel.find(filter)
+}
+async function update(){
+return await orderModel.updateOne()
+}
+async function del(){
+    return await orderModel.updateOne()
+
+}
+
+module.exports={create,read,update,del}
