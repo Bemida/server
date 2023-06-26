800d9845d7012104339cb264832b581fb4ad93c2
Rotem Rogers <rotemrogers@gmail.com>
2023-06-26 10:42:36 +0300

    finallizing the schemas

--- DL/Model/item.model.js
+++ DL/Model/item.model.js
@@ -5,56 +5,14 @@ const itemSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    styling: [
-        {
-            interior: {
-
-                color: { type: String, required: true },
-
-                shelves: {
-                    amount: Number,
-                    position: {
-                        enum: ["right", "left", "center"]
-                    },
-                    hangingRod: {
-                        amount: Number,
-                        position: {
-                            enum: ["right", "left", "center"]
-                        }
-
-                    },
-                    drawers: { amount: Number },
-                    acessories: {
-                        drawerHandles: {
-                            itemNumber: Number,
-                            color: String,
-                            style: String,
-                            img: "🪵"
-                        }
-                    }
-
-                }
-            }
-        },
-        {
-            exterior: {
-                material: { type: String, required: true },
-                doors: { amount: Number },
-                size: { height: Number, width: Number, depth: Number },
-                color: { type: String, required: true },
-                handles: {
-                    itemNumber: Number,
-                    color: String,
-                    style: String,
-                    img: "🪵"
-                },
-                base: {
-                    default: "legs",
-                    enum: ["legs", "tzokel"]
-                }
-
-            }
-        }]
+    img: {
+        url: "🏵️",
+        required : true
+    },
+    stock: {
+        type : Number,
+        default : 1
+    }
 })
 
 const itemModel = mongoose.model("item", itemSchema)
--- DL/Model/order.model.js
+++ DL/Model/order.model.js
@@ -49,7 +49,54 @@ const orderSchema = new mongoose.Schema({
     isActive: {
         type: Boolean,
         default: true
-    }
+    },
+    styling: [{
+        interior: {
+            color: { type: String, required: true },
+                shelves: {
+                    amount: Number,
+                    position: {
+                        enum: ["right", "left", "center"]
+                    },
+                    hangingRod: {
+                        amount: Number,
+                        position: {
+                            enum: ["right", "left", "center"]
+                        }
+
+                    },
+                    drawers: { amount: Number },
+                    acessories: {
+                        drawerHandles: {
+                            itemNumber: Number,
+                            color: String,
+                            style: String,
+                            img: "🪵"
+                        }
+                    }
+
+                }
+            }
+        },
+        {
+        exterior: {
+            material: { type: String, required: true },
+            doors: { amount: Number },
+            size: { height: Number, width: Number, depth: Number },
+            color: { type: String, required: true },
+            handles: {
+                itemNumber: Number,
+                color: String,
+                style: String,
+                img: "🪵"
+            },
+            base: {
+                default: "legs",
+                enum: ["legs", "tzokel"]
+            }
+
+        }
+    }]
 })
 
 const orderModel = mongoose.model("order", orderSchema)
