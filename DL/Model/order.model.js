41d5599bb8c6804dc46626029f572c388df1b6ab
Yehuda Yeger <yeger921@gmail.com>
2023-06-26 14:33:13 +0300

    final order schema

--- DL/Model/order.model.js
+++ DL/Model/order.model.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
     user: {
@@ -19,92 +19,52 @@ const orderSchema = new mongoose.Schema({
         default: "regular",
         enum: ["regular", "express"]
     },
-    stages: [{
-        stage: [{
-            stageNo: {
-                type: Number,
-                default: 1,
-                enum: [1, 2, 3, 4]
-            },
-            furniture: {
-                type: String,
-                enum: ["Closet", "Bed", "Bookcase", "Table", "Console", "Drawers"],
-                default: "Closet"
-            },
-            styling: [{
-                interior: {
-                    color: { type: String, required: true },
-                        shelves: {
-                            amount: Number,
-                            position: {
-                                enum: ["right", "left", "center"]
-                            },
-                            hangingRod: {
-                                amount: Number,
-                                position: {
-                                    enum: ["right", "left", "center"]
-                                }
-        
-                            },
-                            drawers: { amount: Number },
-                            acessories: {
-                                drawerHandles: {
-                                    itemNumber: Number,
-                                    color: String,
-                                    style: String,
-                                    img: String
-                                }
-                            }
-        
-                        }
-                    }
-                },
-                {
-                exterior: {
-                    material: { type: String, required: true },
-                    doors: { amount: Number },
-                    size: { height: Number, width: Number, depth: Number },
-                    color: { type: String, required: true },
-                    handles: {
-                        itemNumber: Number,
-                        color: String,
-                        style: String,
-                        img: "🪵"
-                    },
-                    base: {
-                        default: "legs",
-                        enum: ["legs", "tzokel"]
-                    }
-        
-                }
-            }],
-            items: [{
-                name: {
-                    type: String,
-                    required: true
-                    
-                },
-                price: {
-                    type: Number,
-                    required: true
-                },
-                img: {
-                    type: String,
-                    required: true
-                },
-                stock: {
-                    type : Number,
-                    default : 1
-                },
-                barcode: {
-                    type:Number,
-                    required:true,
-                    unique: true
-                }
-            }
-            ]
-        }]
-    }],
+    furniture: {
+        type: String,
+        enum: ["Closet", "Bed", "Bookcase", "Table", "Console", "Drawers"],
+        default: "Closet"
+    },
+    interiorStyle: {
+        color: { type: String, required: true },
+        shelves: {
+            amount: Number,
+            section: Number,
+            positions: []
+        },
+        hangingRod: {
+            amount: Number,
+            section: Number,
+            positions: []
+        },
+        drawers: {
+            amount: Number,
+            section: Number,
+            positions: []
+        },
+        drawerHandles: {
+            itemNumber: Number,
+            color: String,
+            style: String,
+            img: String
+        }
+    },
+    exteriorStyle: {
+        material: { type: String, required: true },
+        doors: { amount: Number },
+        size: { height: Number, width: Number, depth: Number },
+        color: { type: String, required: true },
+        handles: {
+            itemNumber: Number,
+            color: String,
+            style: String,
+            img: String
+        },
+        base: {
+            type: String,
+            default: "legs",
+            enum: ["legs", "tzokel"]
+        }
+    },
     total: {
         type: Number,
         default: 0
@@ -112,10 +72,9 @@ const orderSchema = new mongoose.Schema({
     isActive: {
         type: Boolean,
         default: true
-    },
-    
-})
+    }
+});
 
-const orderModel = mongoose.model("order", orderSchema)
+const orderModel = mongoose.model("order", orderSchema);
 
-module.exports = orderModel
+module.exports = orderModel;
