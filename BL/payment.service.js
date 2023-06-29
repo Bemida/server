e25928b9b258ab3c44a3f7b5f9c0ab14e56f39b1
j s <grnwtmry@gmail.com>
2023-06-29 11:35:04 +0300

    test

---
+++ BL/payment.service.js
@@ -0,0 +1,15 @@
+async function getFinalPayment(){
+    // const order = 
+    // const finalPayment = 
+    // return finalPayment
+}
+async function getForgingPrice() {
+
+}
+async function getDrawerPrice() {
+
+}
+async function getFormicaCladdingPrice() {
+
+}
+module.exports = {getFinalPayment}
--- Router/payment.route.js
+++ Router/payment.route.js
@@ -1,9 +1,13 @@
 const express = require ('express')
 const router = express.Router()
+const paymentServices = require("../BL/payment.service")
+const { log } = require('console')
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
     try{
+        const order = req.body
         const finalPayment = "ddd"
+        console.log(order)
         res.send(finalPayment)
     }
     catch(err){
